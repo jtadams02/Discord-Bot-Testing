edbd0bae61d1a5418faae5c3b2bdf0579ddc4725
Jacob Adams <95683574+jtadams02@users.noreply.github.com>
2023-05-10 16:12:15 -0500

    done at librarhy

---
+++ PythonForDummies/chapter1.md
@@ -0,0 +1,2 @@
+# Chapter : Starting with Python
+
--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import discord
 from discord.ext import commands
-
 import os
+# Below will handle getting the discord bot's token to log in
+from dotenv import load_dotenv
+load_dotenv()
 # For testing with bungie stuff 
 import aiobungie
+# Random number generator
 import random
-
 # Bungie API Key a7a359696ab84fcebf7a31bb9b2ebddf
 # Probably shouldn't leave that public but whatevs
 bclient = aiobungie.Client('a7a359696ab84fcebf7a31bb9b2ebddf')
-
-
+# Date and Time
 from datetime import date
-
+# Discord needs intents to work for some reason
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-
+# I don't know what code below does
 # Bot variable?
 bot = commands.Bot(command_prefix='$',intents=intents)
 
@@ -25,7 +26,13 @@ async def whois(ctx):
         await ctx.send("I am a discord bot made in Python by the legendary man JT!")
 
 # Bad words will be placed below
-bad_words = ["fuck", "shit", "piss", "bitch", "damn"]
+bad_words = ["fuck", "shit", "piss", "bitch", "damn","pussy","jagoff","wanker","cuck","cock","dick"]
+# And here will be the responses to the bad words
+bad_responses = ["Erm, Language!","That's not gonna fly here buddy","Watch it buster!","I'm warning you kiddo","We don't talk like that around here"]
+
+# Why are these not recognized as variables
+daryl = 0
+jonah = 0
 
 # Responses to truppa
 joe_words = [
@@ -35,17 +42,23 @@ joe_words = [
     "Did anyone ask?"
 ]
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    channel = client.get_channel(789609064967307274)
+    await channel.send("Python Bot Online!")
+
+
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hi'):
-        await message.channel.send('Hello there! I am a robot!')
+    if message.content == '$':
+        await message.channel.send('$ is my prefix. If you\'d like to see more commands enter "$help"')
         
     if message.content.startswith('$hi'):
         await message.channel.send('Hello there! I am a robot!')
@@ -54,10 +67,11 @@ async def on_message(message):
         today = date.today()
         await message.channel.send(today)
         return
-        
+
+    # Below code checks whether or not a word in the message matches a word in bad_words    
     if any(word in message.content for word in bad_words): 
-        await message.channel.send("Erm, language!")
-        return
+        await message.channel.send(random.choice(bad_responses)).then(r => r.delete({ timeout: 5000}))
+        return      
         
     # Joe Code
     if message.author.id == 344659595970740226:
@@ -65,6 +79,14 @@ async def on_message(message):
         if random_number == 1: 
             await message.channel.send(random.choice(joe_words))
         return 
+    
+    # Jonah Code
+    if message.author.id == 340542697909518336:
+        if jonah == 1:
+            jonah = 0
+            await message.channel.send("Hello Jonah! I am a Robot!")
+        return
+        
         
     # JT Code
     #if message.author.id == 173748750068482048:
@@ -73,7 +95,10 @@ async def on_message(message):
     #        await message.channel.send("Random number found!")
     #    return 
         
-    if message.author.id == 447520381218193408:
-        await message.channel.send("Hi daryl")
+    if message.author.id == 173748750068482048:
+        if jonah == 1:
+            jonah = 0
+            await message.channel.send("Hi Daryl, I am a robot!")
+        return
         
-client.run('MTEwNTcwODkwOTgzNjEyODM2Ng.GfcnBr.d_xsQInizoxhM8_HnL71ebq_RCVHtZa6GI3rRA')
+client.run(os.getenv('BOT_TOKEN'))
