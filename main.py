15ca64a3cd56b5c232b4077d674214d7c257aee5
Jacob Adams <95683574+jtadams02@users.noreply.github.com>
2023-05-11 14:28:26 -0500

    learned how to reply to messages

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     channel = client.get_channel(789609064967307274)
     startTime = time.time()
-    await channel.send("Raspberry Py Online!")
+    # await channel.send("Raspberry Py Online!")
 
 
 
@@ -63,7 +63,7 @@ async def on_message(message):
         return
     # This is just the prefix command. I know I'm not doing commands right but I'm learning okay
     if message.content == '$':
-        await message.channel.send("`$ is my prefix. If you\'d like to see more commands enter \"$help\"`")
+        await message.channel.send("`$ is my prefix. If you\'d like to see more commands enter \"$help\"\nI am a bot coded in Python by JT`")
         return
 
     # Simple help command
@@ -74,19 +74,22 @@ async def on_message(message):
     # Ideally this will add the given swear to the swear list. Jt should maintain the swear list to make sure it is right
     if message.content.startswith("$swear"):
         msg = message.content
+        if len(msg.split()) < 2:
+            await message.reply("We're sorry, but we cannot add an empty space to the list",delete_after=5.0)
+            return
         swear = msg.split(' ')[1]
         if swear.isspace():
-            await message.channel.send("We're sorry, but we cannot add an empty space to the list")
+            await message.reply("We're sorry, but we cannot add an empty space to the list",delete_after=5.0)
             return
         temp = f # Temp variable I do not think is needed!
         if swear.lower() in bad_words:
-            await message.channel.send("We're sorry, but that swear is already on the list!",delete_after=5.0)
+            await message.reply("We're sorry, but that swear is already on the list!",delete_after=5.0)
             return
         else:
             temp.write('\n'+swear.lower())
             bad_words.append(swear.lower())
             toSend = (swear+' has been added to the swearlist. Thank you for making our server a safer place!')
-            await message.channel.send(toSend,delete_after=5.0)
+            await message.channel.send(toSend)
             return
 
     if message.content == '$uptime':
@@ -107,16 +110,16 @@ async def on_message(message):
     # delete_after causes the message to delete after 5 seconds!
     if any(word in message.content.lower() for word in bad_words):
             if message.author.id == 743721232326852628:
-                await message.channel.send(random.choice(in_the_clear),delete_after=5.0)
+                await message.reply(random.choice(in_the_clear),delete_after=5.0)
                 return
             else:
-                await message.channel.send(random.choice(bad_responses),delete_after=5.0)
+                await message.reply(random.choice(bad_responses),delete_after=5.0)
                 return
-    # Joe Code
+    # Joe Code, just respondes to joe lol
     if message.author.id == 344659595970740226:
         random_number = random.randint(1,5)
         if random_number == 1:
-            await message.channel.send(random.choice(joe_words))
+            await message.reply(random.choice(joe_words,delete_after=5.0))
         return
 
 client.run(os.getenv('BOT_TOKEN'))
