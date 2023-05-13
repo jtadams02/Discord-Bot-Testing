67d4ee7d7e21b01dee80b57daea39c62ac7d7290
Jacob Adams <95683574+jtadams02@users.noreply.github.com>
2023-05-12 20:13:47 -0500

    added a deletion and shutdown, list now private

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import os
+# Signal will allow us to update our swearlist on CTRL+C
+import signal
 # Below will handle getting the discord bot's token to log in
 from dotenv import load_dotenv
 load_dotenv()
@@ -19,7 +21,8 @@ import datetime,time
 bad_words = []
 f = open('swearlist.txt','r+')
 for line in f:
-    bad_words.append(line.strip().lower())
+    if not line.strip().lower().isspace():
+        bad_words.append(line.strip().lower())
 # And here will be the responses to the bad words
 bad_responses = ["Erm, Language!","That's not gonna fly here buddy","Watch it buster!","I'm warning you kiddo","We don't talk like that around here"]
 # And here will be the responses to the people who are excluded
@@ -27,7 +30,7 @@ in_the_clear = ["I didn't see anything","I'll let it slide this time","She gets 
 # Responses to truppa
 joe_words = [
     "Not funny",
-    "Try again truppa",
+    "Try again tman",
     "No one cares",
     "Did anyone ask?"
 ]
@@ -52,7 +55,7 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     channel = client.get_channel(789609064967307274)
     startTime = time.time()
-    # await channel.send("Raspberry Py Online!")
+    await channel.send("Raspberry PyBot Online!")
 
 
 
@@ -68,9 +71,28 @@ async def on_message(message):
 
     # Simple help command
     if message.content == '$help':
-        await message.channel.send("`There aren't a lot of commands right now, but here's what we got:\n$swear : Report a swear word you would like to add to the no-no list`")
+        await message.channel.send("`There aren't a lot of commands right now, but here's what we got:\n$swear : Report a swear word you would like to add to the no-no list\n$delete : Delete a swear word from the swear list\n$list : List all of the swears in the swearlist!\n$shutdown : Turn off the discord bot (Only JT can run this)`")
         return
 
+    # I'm gonna make the shutdown command run first due to priority reasons
+    if message.content == '$shutdown':
+        if message.author.id == 173748750068482048:
+            await client.close()
+            return # Is this needed?
+        else:
+            message.reply('Sorry, but only King JT can run this command! Better luck next time')
+            return
+    
+    # List the swear list
+    if message.content == '$list':
+        # Loop time to print
+        response1 = '`'
+        for word in bad_words:
+            response1+=(word+'\n')
+        response1+='`'
+        await message.channel.send(response1)
+        return
+            
     # Ideally this will add the given swear to the swear list. Jt should maintain the swear list to make sure it is right
     if message.content.startswith("$swear"):
         msg = message.content
@@ -86,11 +108,35 @@ async def on_message(message):
             await message.reply("We're sorry, but that swear is already on the list!",delete_after=5.0)
             return
         else:
-            temp.write('\n'+swear.lower())
+            #temp.write('\n'+swear.lower())
             bad_words.append(swear.lower())
             toSend = (swear+' has been added to the swearlist. Thank you for making our server a safer place!')
             await message.channel.send(toSend)
             return
+        
+    # This code will remove swears from the bad_words array and ideally the file    
+    if message.content.startswith("$delete"):
+        msg = message.content 
+        if len(msg.split()) < 2:
+            await message.reply("Sorry, but swears are only 1 word long!",delete_after=5.0)
+            return
+        swear = msg.split(' ')[1]
+        if swear.isspace():
+            await message.reply("Cannot delete empty string",delete_after=5.0)
+            return
+        if swear.lower() in bad_words:
+            # Now we remove the swear
+            while(swear.lower() in bad_words):
+                bad_words.remove(swear)
+            response = swear +" has been deleted!"
+            await message.reply(response)
+            return
+            #IDK how to remove it from file yet?!
+        else: 
+            await message.reply("We're sorry, but that swear is not in the swear list!")
+            return
+
+            
 
     if message.content == '$uptime':
         uptime = str(datetime.timedelta(seconds=int(round(time.time()-startTime))))
@@ -115,11 +161,49 @@ async def on_message(message):
             else:
                 await message.reply(random.choice(bad_responses),delete_after=5.0)
                 return
-    # Joe Code, just respondes to joe lol
-    if message.author.id == 344659595970740226:
-        random_number = random.randint(1,5)
+    # tman, just respondes to tman lol
+    if message.author.id == 1106741623943086090:
+        random_number = random.randint(1,2)
         if random_number == 1:
             await message.reply(random.choice(joe_words),delete_after=5.0)
         return
 
+# Below 2 functions will run on a disconnect from wifi, not when CTRL+C is pressed
+@client.event
+async def on_disconnect():
+    # This should run when the discord bot disconnects from the server. At this time I will have it update the swearlist file!
+    print('Regular disconnect')
+    # We need to update swear list!
+    f.close()
+    rewriteFile = open('swearlist.txt','w')
+    for i in bad_words:
+        rewriteFile.write(i.lower()+'\n')
+        print(i.lower())
+    # Should be perfect
+
+@client.event
+async def on_shard_disconnect():
+    # This should run when the discord bot disconnects from the server. At this time I will have it update the swearlist file!
+    print("Shard Disconnect")
+    f.close()
+    rewriteFile = open('swearlist.txt','w')
+    for i in bad_words:
+        rewriteFile.write('\n'+i.lower())
+        print ("Writing: " + i.lower + " to file!")
+    return
+
+# Handler for CTRL+C, thanks stack overflow
+def signal_handler(sig,frame):
+    print("CTRL+C Detected")
+    f.close()
+    rewriteFile = open('swearlist.txt','w')
+    for i in bad_words:
+        rewriteFile.write('\n'+i.lower())
+        print ("Writing: " + i.lower + " to file!")
+    # This code kills program
+    sys.exit(0)
+
 client.run(os.getenv('BOT_TOKEN'))
+signal.signal(signal.SIGINT, signal_handler)
+#signal.pause() THIS CODE DOESNT WORK ON WINDOWS!
+
